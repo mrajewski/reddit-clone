55365586d0a9c7cd13b3db9d3213e2a11bd2ce6c
Maciej Rajewski <rajewski.ma@gmail.com>
2022-04-16 16:02:09 +0200

    app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/provider";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-
+      <ChakraProvider>
+        <Component {...pageProps}/>
+      </ChakraProvider>
   )
 }
 
